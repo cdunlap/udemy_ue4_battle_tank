d8d394641cf38a4bb615987e7aa9f65c391cfcbf
Cale Dunlap <cale.dunlap@gmail.com>
2017-08-06 23:39:59 -0700

    Tank AI controller and default to AI possessing the world tanks

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "TankPlayerController.h"
+#include "TankAIController.h"
 
-void ATankPlayerController::BeginPlay()
+void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("PlayerController begin play"));
+	UE_LOG(LogTemp, Warning, TEXT("AIController begin play"));
 
 	ATank * tank = GetControlledTank();
 	if (tank == nullptr)
@@ -15,11 +15,11 @@ void ATankPlayerController::BeginPlay()
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Player controlled tank: %s"), *tank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("AI posesing tank: %s"), *tank->GetName());
 	}
 }
 
-ATank * ATankPlayerController::GetControlledTank() const
+ATank * ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
