5ba712614c5bc31970facfb41e18770b9e2f834b
Cale Dunlap <cale.dunlap@gmail.com>
2017-08-06 23:57:42 -0700

    Moved some files around

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -16,8 +16,9 @@ class BATTLETANK_API ATankAIController : public AAIController
 	GENERATED_BODY()
 	
 public:
-	virtual void BeginPlay() override;
+	void BeginPlay() override;
 	
+private:
 	ATank * GetControlledTank() const;
 
 	ATank * GetPlayerTank() const;
